2f9b150f29c06d0cec582d46178ac42ece77575d
KevinPaez28 <Kevinandreypaezgarces@gmail.com>
2025-03-19 00:26:03 -0500

    Renombre de archivo y fetch

--- modulos/Tareas/index.js
+++ modulos/Tareas/index.js
@@ -1,3 +1,3 @@
-export { getTareas } from "./Solicitar_tareas.js";//exportamos la funcion getTareas desde Solicitar_tareas.js
-export { getTareaspendientes } from "./Solicitar_tareas_pendientes.js";
+export { getTareas } from "./solicitar_tareas.js";//exportamos la funcion getTareas desde Solicitar_tareas.js
+export { getTareaspendientes } from "./solicitar_tareas_pendientes.js";
 //exportamos la funcion getTareaspendientes desde Solicitar_tareas_pendientes.js
